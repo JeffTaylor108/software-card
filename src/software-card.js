a86b4e2f107c64fc02a7352c6afa2bbe722941c0
JeffTaylor108 <jeffta1080@gmail.com>
2023-02-16 13:25:58 -0500

    commit

--- src/software-card.js
+++ src/software-card.js
@@ -1,10 +1,11 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL("https://pbs.twimg.com/profile_images/1496512709348798464/-19Uii-a_400x400.jpg", import.meta.url).href;
 
-class WebComponentCard extends LitElement {
+class SoftwareCard extends LitElement {
   static properties = {
-    header: { type: String },
+    name: { type: String, },
+    details: { type: String, }
   };
 
   static styles = css`
@@ -110,45 +111,27 @@ class WebComponentCard extends LitElement {
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.name = "Heading";
+    this.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
   }
 
   render() {
     return html`
     <div class="cardContainer">
     <div class="card">
-      <h1 class="heading"> Heading </h1>
+      <h1 class="heading"> ${this.name} </h1>
       <div class="container">
-          <img class ="image" src="https://pbs.twimg.com/profile_images/1496512709348798464/-19Uii-a_400x400.jpg">
+          <img class ="image" src="${logo}">
         <p class="paragraph">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          ${this.details}
+        </p>
       </div>
       <div>
         <button id="newDetailsButton" class="detailsButton"> Details </button>
       </div>
     </div>
-    
-    <div class="card">
-      <h1 class="heading"> Heading </h1>
-      <div class="container">
-          <img class ="image" src="https://pbs.twimg.com/profile_images/1496512709348798464/-19Uii-a_400x400.jpg">
-        <p class="paragraph">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-      </div>
-      <div>
-        <button class="detailsButton"> Details </button>
-      </div>
-    </div>
-    </div>
-    <div class="buttons">
-      <button class="cloneButton">Clone Page </button>
-    <button class="backgroundButton"> Toggle Card Background Color</button>
-    <button class="headingButton"> Change Heading Text</button>
-      <button class="deleteLastButton"> Delete Last Clone</button>
-    </div>
-      
     `;
   }
 }
 
-customElements.define('web-component-card', WebComponentCard);
+customElements.define('software-card', SoftwareCard);
