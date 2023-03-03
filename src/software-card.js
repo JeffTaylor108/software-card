aaa3a7f54f8c5bafda52efd9e99e7022d6b5aa4e
JeffTaylor108 <jeffta1080@gmail.com>
2023-03-03 17:42:14 -0500

    commit

--- src/software-card.js
+++ src/software-card.js
@@ -143,13 +143,14 @@ details {
         </meme-maker>
         <details class="details">
           <summary>${this.softwareDetails}</summary>
-          <div>
-            <slot>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </slot>
+          </p>
+          <div>
+            <slot></slot>
           </div>
         </details>
       </div>
