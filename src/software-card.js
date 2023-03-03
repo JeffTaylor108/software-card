15c2133d96bd103c587baef4d48eacaefe9a56cb
JeffTaylor108 <jeffta1080@gmail.com>
2023-03-03 17:31:58 -0500

    commit

--- src/software-card.js
+++ src/software-card.js
@@ -144,7 +144,12 @@ details {
         <details class="details">
           <summary>${this.softwareDetails}</summary>
           <div>
-            <slot></slot>
+            <slot>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </slot>
           </div>
         </details>
       </div>
