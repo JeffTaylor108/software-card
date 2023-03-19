8378c3a15ca4721dfe4b07a7035f50d1a3dd61bc
JeffTaylor108 <jeffta1080@gmail.com>
2023-03-19 14:26:31 -0400

    commit

--- src/software-roster.js
+++ src/software-roster.js
@@ -20,7 +20,7 @@ export class SoftwareRoster extends LitElement {
     }
 
     updateRoster() {
-        const address = new URL('../api/roster.js', import.meta.url).href;
+        const address = new URL('../assets/roster.json', import.meta.url).href;
         fetch(address).then((response) => {
             if (response.ok) {
                 return response.json()
