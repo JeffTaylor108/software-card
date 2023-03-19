87c218e5a09cafd3f25d5976a542eb95f929bad0
JeffTaylor108 <jeffta1080@gmail.com>
2023-03-19 14:23:28 -0400

    commit

---
+++ api/roster.js
@@ -0,0 +1,25 @@
+export default async function handler() {
+
+    const roster = [
+      {
+        "name": "Apple",
+        "subheader": "a company",
+        "softwareDetails": "Details",
+        "top": "Apple",
+        "bottom": "Apple"
+      },
+      {
+        "name": "Definitely not Apple",
+        "subheader": "not a company",
+        "softwareDetails": "Details",
+        "top": "Apple",
+        "bottom": "Apple"
+      }
+    ];
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.json(roster);
+  }
